31aaed04fc956a8009e7fe25f79d6ac551a1abf2
JT1997 <jttiwari97@gmail.com>
2024-06-05 11:45:57 +0530

    changes to just even numbers

--- Code.py
+++ Code.py
@@ -1,7 +1,9 @@
 #Sum of even number from 1-10
 def sum():
     s=0
-    for i in range (1,11):
-        s=i+s
-    print (s)
+   for i in range (1,11):
+        if i%2==0: 
+            s=i+s
+    print(s)
 sum()
+
